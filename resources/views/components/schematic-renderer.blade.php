6c370bc59b65cbfc80b8e0c372e406f50b2417dd
Nano112 <harrisonbastian.nano@gmail.com>
2024-02-23 15:26:20 +0000

    Update .gitignore and schematic-renderer.blade.php

--- resources/views/components/schematic-renderer.blade.php
+++ resources/views/components/schematic-renderer.blade.php
@@ -1,11 +1,26 @@
-<div>
-    <canvas id="canvas-{{ $schematic->string_id }}" width="400" height="400" wire:ignore>
-
-    </canvas>
+<div class="aspect-w-4 aspect-h-3 w-full min-h-96">
+    <canvas id="canvas-{{ $schematic->string_id }}" wire:ignore class="w-full h-full"></canvas>
 </div>
+
+
+@once
+    @push('scripts')
+        <script>
+            function setCanvasDimensions(canvasId) {
+                const canvas = document.getElementById(canvasId);
+                const container = canvas.parentElement;
+                canvas.width = container.offsetWidth;
+                canvas.height = container.offsetHeight;
+            }
+        </script>
+    @endpush
+@endonce
+
 
 @push('scripts')
     <script type="module">
+        setCanvasDimensions('canvas-{{ $schematic->string_id }}');
+        window.addEventListener('resize', () => setCanvasDimensions('canvas-{{ $schematic->string_id }}'));
         const schematic_{{ $schematic->string_id }} = @json($schematic->base64);
         const canvas_{{ $schematic->string_id }} = document.getElementById('canvas-{{ $schematic->string_id }}');
         const renderer_{{ $schematic->string_id }} = new SchematicRenderer.SchematicRenderer(
