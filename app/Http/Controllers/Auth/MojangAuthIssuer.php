92a1f506fcc4c2dc409752f1c22634881bd7b323
Matthias Wijnsma <matthiasx95@gmail.com>
2024-03-31 12:33:11 +0200

    implement mojang auth issuer

--- app/Http/Controllers/Auth/MojangAuthIssuer.php
+++ app/Http/Controllers/Auth/MojangAuthIssuer.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Helpers\JWT;
+use GuzzleHttp\Client;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\MojangAuthIssuerRequest;
 
@@ -12,6 +14,25 @@ class MojangAuthIssuer extends Controller
      */
     public function __invoke(MojangAuthIssuerRequest $request)
     {
-        //
+        $client = new Client();
+        $response = $client->get("https://sessionserver.mojang.com/session/minecraft/hasJoined?username={$request->username}&serverId={$request->serverId}");
+
+        if ($response->getStatusCode() !== 200) {
+            return response()->json(
+                [
+                    'error' => 'Mojang API returned an error',
+                ],
+                401
+            );
+        }
+
+        $result = json_decode($response->getBody(), true);
+
+        return response()->json([
+            'token' => JWT::getToken([
+                'username' => $result['name'],
+                'uuid' => $result['id'],
+            ])
+        ], 200);
     }
 }
